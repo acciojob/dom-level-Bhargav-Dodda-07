6f8106ada17c702e24cf1b977c18374feb9609f4
DODDA BHARGAV <128250258+Bhargav-Dodda-07@users.noreply.github.com>
2023-09-28 21:08:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+// Function to calculate the DOM level of an element by ID
+function calculateDOMLevelById(id) {
+  const element = document.getElementById("level");
+  
+  if (!element) {
+    alert("Element with ID not found.");
+    return;
+  }
+
+  let domLevel = 1;
+  let currentElement = element;
+
+  while (currentElement !== document.documentElement) {
+    currentElement = currentElement.parentElement;
+    domLevel++;
+  }
+
+  alert(`The level of the element is: ${domLevel}`);
+}
+
+// Call the function with the ID "level"
+calculateDOMLevelById("level");
